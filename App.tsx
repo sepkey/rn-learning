300c5f9d548092c3707dae10c726e8491f817db9
Sepide Kia <sepidekia6990@gmail.com>
2025-08-28 12:59:12 +0330

    feat: style the text

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello sepide world</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: "#2743de",
+          paddingHorizontal: 8,
+          paddingVertical: 16,
+        }}
+      >
+        <Text style={{ fontSize: 18, fontWeight: "200" }}>Coffee</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#d54d4d",
-    alignItems: "center",
+    backgroundColor: "#fff",
     justifyContent: "center",
   },
 });
