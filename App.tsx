2e47b71bf03a0ca248242d6674e5b98ac9221061
Sepide Kia <sepidekia6990@gmail.com>
2025-08-28 20:32:54 +0330

    feat: add alert and the button for triggering it

--- App.tsx
+++ App.tsx
@@ -1,18 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { theme } from "./theme";
 
 export default function App() {
+  const handleDlete = () => {
+    Alert.alert("Are you sure to do this?", "delete the item", [
+      {
+        text: "Yes",
+        onPress: () => console.log("Ok,deleting"),
+        style: "destructive",
+      },
+      {
+        text: "Cancel",
+        onPress: () => console.log("No,Calncel"),
+        style: "cancel",
+      },
+    ]);
+  };
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          borderBottomWidth: 1,
-          borderBottomColor: theme.colorCelurean,
-          paddingHorizontal: 8,
-          paddingVertical: 16,
-        }}
-      >
-        <Text style={{ fontSize: 18, fontWeight: "200" }}>Coffee</Text>
+      <View style={styles.itemContainer}>
+        <Text style={{ fontSize: 18, fontWeight: "200" }}>Coffee</Text>{" "}
+        <TouchableOpacity style={styles.button} onPress={handleDlete}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -23,5 +33,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCelurean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    textTransform: "uppercase",
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    letterSpacing: 1,
   },
 });
--- theme.ts
+++ theme.ts
@@ -1,4 +1,5 @@
 export const theme = {
   colorCelurean: "#2743de",
   colorWhite: "#fff",
+  colorBlack: "#000",
 };
