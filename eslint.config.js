3901767de9fcceeeb8448d9fc391668a11b93ff5
Sepide Kia <sepidekia6990@gmail.com>
2025-08-29 08:23:20 +0330

    chore: add react native eslint plugin, and active error for unused styles

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,18 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const prettier = require("eslint-plugin-prettier/recommended");
+const reactNative = require("eslint-plugin-react-native");
 
 module.exports = defineConfig([
   expoConfig,
   prettier,
   {
     ignores: ["dist/*"],
+    plugins: {
+      "react-native": reactNative,
+    },
+    rules: {
+      "react-native/no-unused-styles": "error",
+    },
   },
 ]);
