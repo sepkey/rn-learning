d4107dec3109525a9f1b5904f4e20c46dd49b214
Sepide Kia <sepidekia6990@gmail.com>
2026-03-12 20:30:40 +0330

    feat: add the input and state for it

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,21 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
 import ShoppingListItem from "../components/shopping-list-item";
 import { theme } from "../theme";
 
 export default function App() {
+  const [value, setValue] = useState("");
   return (
     <View style={styles.container}>
+      <TextInput
+        value={value}
+        placeholder="E.g. Coffee"
+        style={styles.textInput}
+        onChangeText={setValue}
+        // keyboardType="name-phone-pad" //you can select the keyboard type for the inut
+        returnKeyType="done" //you can change the text of return button
+        onSubmitEditing={() => console.log("submitted")}
+      />
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Soda" />
@@ -16,6 +27,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
