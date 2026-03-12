f0fb94729fdfa2bd1002a93ef3111f466925183b
Sepide Kia <sepidekia6990@gmail.com>
2026-03-12 16:09:04 +0330

    feat: add history screen to one of the tabs of bottom tab navigation

--- app/counter/_layout.tsx
+++ app/counter/_layout.tsx
@@ -1,9 +1,21 @@
-import { Stack } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
+import { theme } from "../../theme";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Counter" }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Counter",
+          headerRight: () => (
+            <Link href="/counter/history">
+              <MaterialIcons size={32} name="history" color={theme.colorGrey} />
+            </Link>
+          ),
+        }}
+      />
     </Stack>
   );
 }
