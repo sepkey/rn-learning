ac9ec99fc64849373569859dd4bb40ae231e077f
Sepide Kia <sepidekia6990@gmail.com>
2026-03-12 11:38:13 +0330

    feat: change the icon of bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,38 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { Tabs } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Shopping List" }} />
-      <Tabs.Screen name="counter" options={{ title: "Counter" }} />
-      <Tabs.Screen name="idea" options={{ title: "Idea" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
