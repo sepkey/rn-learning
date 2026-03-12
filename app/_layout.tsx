a5acf6752b78aebf7bacc6f98c9f7d37861b0fc7
Sepide Kia <sepidekia6990@gmail.com>
2026-03-12 11:18:28 +0330

    feat: add the modal navigation (with the same view on android and different view on IOS)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,22 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping List" }} />
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
     </Stack>
   );
 }
