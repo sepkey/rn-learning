f5dd5fb6c2f8c3ad1090c47b3f208ed8b41d8f8f
Sepide Kia <sepidekia6990@gmail.com>
2026-03-23 00:25:48 +0330

    feat: add the history logs list

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,21 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
+import { format } from "date-fns";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import useCountdownState from "../../components/hooks/use-countdown-state";
+import { theme } from "../../theme";
+
+const fullDateFormat = "LLL d yyy, h:mm aaa";
 
 export default function HistoryScreen() {
+  const { countdownState } = useCountdownState();
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      data={countdownState?.completedAtTimestamps}
+      renderItem={({ item }) => (
+        <View style={styles.listItem}>
+          <Text style={styles.listItemText}>
+            {format(item, fullDateFormat)}
+          </Text>
+        </View>
+      )}
+      ListEmptyComponent={() => (
+        <View style={styles.listEmptyContainer}>
+          <Text>No history.</Text>
+        </View>
+      )}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  list: { flex: 1, backgroundColor: theme.colorWhite },
+  contentContainer: { marginTop: 8 },
+  listEmptyContainer: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    marginVertical: 18,
   },
-  text: {
-    fontSize: 24,
+  listItem: {
+    backgroundColor: theme.colorLightGrey,
+    marginHorizontal: 8,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 8,
+  },
+  listItemText: {
+    fontSize: 18,
   },
 });
