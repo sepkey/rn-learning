25b8e3816491db61a467570bd35285fa757b295c
Sepide Kia <sepidekia6990@gmail.com>
2026-03-24 19:30:33 +0330

    feat: persist onboarding state via  zustand

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,15 +1,29 @@
+/* eslint-disable prettier/prettier */
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 type UserStore = {
   hasFinishedOnboarding: boolean;
   toggleHasOnboarded: () => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-  hasFinishedOnboarding: false,
-  toggleHasOnboarded: () => {
-    set((state) => {
-      return { ...state, hasFinishedOnboarding: !state.hasFinishedOnboarding };
-    });
-  },
-}));
+export const useUserStore = create(
+  persist<UserStore>(
+    (set) => ({
+      hasFinishedOnboarding: false,
+      toggleHasOnboarded: () => {
+        set((state) => {
+          return {
+            ...state,
+            hasFinishedOnboarding: !state.hasFinishedOnboarding,
+          };
+        });
+      },
+    }),
+    {
+      name: "user-store",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
