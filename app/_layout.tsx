bd920332713f728766dea3006a7fcf55c67af441
Sepide Kia <sepidekia6990@gmail.com>
2026-03-24 10:26:47 +0330

    chore:start new plantly project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,40 +1,14 @@
-import AntDesign from "@expo/vector-icons/AntDesign";
-import Feather from "@expo/vector-icons/Feather";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import { Tabs } from "expo-router";
-import { theme } from "../theme";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCelurean }}>
-      <Tabs.Screen
+    <Stack>
+      <Stack.Screen
         name="index"
         options={{
-          title: "Shopping List",
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="list" size={size} color={color} />
-          ),
+          title: "Home",
         }}
       />
-      <Tabs.Screen
-        name="counter"
-        options={{
-          title: "Counter",
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="clockcircleo" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="idea"
-        options={{
-          title: "Idea",
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="lightbulb" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    </Stack>
   );
 }
